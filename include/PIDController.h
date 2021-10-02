86d4409ae0f2da05c7755ddd6d86c8b2b7c8fd5b
mjoshi07 <mayankjoshi63gmail.com>
2021-10-02 13:04:47 -0400

    initializeParameters method access changed to private

--- include/PIDController.h
+++ include/PIDController.h
@@ -20,65 +20,68 @@
 
 class PIDController {
  public:
-    /**
-     * @brief Constructs a new PIDController object with randomly initialized kp, ki, kd, dt and error values
-     * 
-     * @param Nothing
-     * 
-     */
-    PIDController();
-    PIDController(double &kp1, double &ki1, double &kd1);
-    ~PIDController();
+   /**
+    * @brief Constructs a new PIDController object with randomly initialized kp, ki, kd, dt and error values
+    * 
+    * @param Nothing
+    * 
+    */
+   PIDController();
+   PIDController(double &kp1, double &ki1, double &kd1);
+   ~PIDController();
 
-    /**
-     * @brief computes a new velocity using the target_velocity, current_velocity and PID parameters
-     * 
-     * @param target_velocity 
-     * @param current_velocity 
-     * 
-     * @return double new_velocity
-     */
-    double compute(double &target_vel, double& actual_vel);
-    /**
-     * @brief Get the PID gain parameters
-     * 
-     * @param Nothing
-     * 
-     * @return std::tuple<double, double, double> (kp, ki, kd)
-     */
-    std::tuple<double, double, double> getGainParameters() const;
-     /**
-     * @brief Set the PID parameters to default values
-     * 
-     * @param Nothing
-     * 
-     * @return Nothing
-     */
-      void initializeParameters();
-     /**
-     * @brief Set the PID parameters
-     * 
-     * @param kp 
-     * @param ki 
-     * @param kd 
-     * 
-     * @return Nothing
-     */
-      void initializeParameters(double &kp, double &ki, double &kd);
-      /**
-       * @brief Get the calculated error
-       * 
-       * @return double error
-       */
-      double getError() const;
+   /**
+    * @brief computes a new velocity using the target_velocity, current_velocity and PID parameters
+    * 
+    * @param target_velocity 
+    * @param current_velocity 
+    * 
+    * @return double new_velocity
+    */
+   double compute(double &target_vel, double& actual_vel);
+   /**
+    * @brief Get the PID gain parameters
+    * 
+    * @param Nothing
+    * 
+    * @return std::tuple<double, double, double> (kp, ki, kd)
+    */
+   std::tuple<double, double, double> getGainParameters() const;
+   /**
+    * @brief Get the calculated error
+    * 
+    * @return double error
+    */
+   double getError() const;
 
  private:
-    /// member to store PID gain parameters
-    double kp_, ki_, kd_;
-    /// member to store computed velocity
-    double new_velocity_;
-    /// member to store time step and error
-    double dt_, error_, prev_err_;
+   /**
+    * @brief Set the PID parameters to default values
+    * 
+    * @param Nothing
+    * 
+    * @return Nothing
+    */
+   void initializeParameters();
+   /**
+    * @brief Set the PID parameters
+    * 
+    * @param kp 
+    * @param ki 
+    * @param kd 
+    * 
+    * @return Nothing
+    */
+   void initializeParameters(double &kp, double &ki, double &kd);
+
+
+ private:
+   /// member to store PID gain parameters
+   double kp_, ki_, kd_;
+   /// member to store computed velocity
+   double new_velocity_;
+   /// member to store time step and error
+   double dt_, error_, prev_err_;
 };
 
 
