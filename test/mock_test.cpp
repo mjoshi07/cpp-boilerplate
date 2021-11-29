7e2c4954250d736a966ca10d21d739e9d1e7c16f
mjoshi07 <mayankjoshi63@gmail.com>
2021-11-29 15:31:09 -0500

    added tests for mocked class

---
+++ test/mock_test.cpp
@@ -0,0 +1,38 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <MockSensor.hpp>
+#include <AnalogSensor.hpp>
+
+
+using namespace std;
+using ::testing::AtLeast;
+using ::testing::Return;
+using ::testing::_;
+
+TEST(MockSensorTest, Read_success) {
+    // Arrange
+    MockSensor ms;
+    AnalogSensor ans(ms);
+    
+    EXPECT_CALL(ms, sensor_val()).Times(1).WillOnce(Return(1));
+
+    // Act
+    int retVal = ans.Read(1);
+
+    // Assert
+    EXPECT_EQ(retVal, 1);
+}
+
+TEST(MockSensorTest, Read_failure) {
+    // Arrange
+    MockSensor ms;
+    AnalogSensor ans(ms);
+    
+    EXPECT_CALL(ms, sensor_val()).Times(1).WillOnce(Return(-1));
+
+    // Act
+    int retVal = ans.Read(-10);
+
+    // Assert
+    EXPECT_EQ(retVal, -1);
+}
