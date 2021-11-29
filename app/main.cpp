2b5558b15977a1f996c26bf7d528ca01e984d585
mjoshi07 <mayankjoshi63@gmail.com>
2021-11-29 15:32:00 -0500

    updated CMakeLists for app

--- app/main.cpp
+++ app/main.cpp
@@ -1,14 +1,27 @@
+/**
+ * @file main.cpp
+ * @author mayank joshi
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include <iostream>
+#include <memory>
+#include <Sensor.hpp>
 #include <AnalogSensor.hpp>
 
-int main()
-{
-    AnalogSensor lightSensor(5);
-    std::cout << "Averaged sensor reading: " << lightSensor.Read() << std::endl;
-    bool terminator = true;
-    if( terminator )
-    {
-        std::cout << "DONE" << std::endl;
-    }
+
+int main() {
+    Sensor generic_sensor;
+    AnalogSensor analog_sensor(generic_sensor);
+
+    int num_samples = 10;
+
+    std::cout << "value: " << analog_sensor.Read(num_samples) << std::endl;
+
     return 0;
 }
